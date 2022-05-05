610012cd0f8305b64cbe0e9171d95cba0a4073f6
“Oleksandr <oleksandr_lopatin@epam.com>
2022-05-05 19:00:10 +0300

    task 3 solved

--- task03-map.go
+++ task03-map.go
@@ -4,7 +4,7 @@ import "sort"
 
 func sortMapValues(input map[int]string) (result []string) {
 	//Place your code here
-	keys := make([]int, 0, len(input))
+	keys := make([]int, len(input), len(input))
 	for k := range input {
 		keys = append(keys, k)
 	}
