e85e05ccdac0f601681942a466040d5e4ed6f666
“Oleksandr <oleksandr_lopatin@epam.com>
2022-05-05 19:27:19 +0300

    watch lesson again and fit after testing

--- task03-map.go
+++ task03-map.go
@@ -2,16 +2,35 @@ package homework
 
 import "sort"
 
-func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
-	keys := make([]int, len(input), len(input))
-	for k := range input {
-		keys = append(keys, k)
-	}
-	sort.Ints(keys)
+type keyValue struct {
+	key   int
+	value string
+}
 
-	for _, k := range keys {
-		result = append(result, input[k])
+type mapValues []keyValue
+
+func (mv mapValues) Len() int {
+	return len(mv)
+}
+
+func (mv mapValues) Less(i, j int) bool {
+	return mv[i].key < mv[j].key
+}
+
+func (mv mapValues) Swap(i, j int) {
+	mv[i], mv[j] = mv[j], mv[i]
+}
+
+func sortMapValues(input map[int]string) (result []string) {
+	l := len(input)
+	mapVals := make([]keyValue, 0, l)
+	for k, v := range input {
+		mapVals = append(mapVals, keyValue{k, v})
+	}
+	sort.Sort(mapValues(mapVals))
+	result = make([]string, l)
+	for i, mv := range mapVals {
+		result[i] = mv.value
 	}
 	return result
 }
